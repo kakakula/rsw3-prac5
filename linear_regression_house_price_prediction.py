5f691d034edd3669865e0956835998b15b725e7a
kakakula <111406208+kakakula@users.noreply.github.com>
2025-07-29 15:48:55 +0800

    Add files via upload

---
+++ linear_regression_house_price_prediction.py
@@ -0,0 +1,5 @@
+model = load('linear_regression_model.joblib')
+user_input = int(input ("Enter house size:"))
+input_array = np.array([user_input]).reshape(-1, 1)
+predicted_price = model.predict(input_array)
+print(f"Predicted Value: {predicted_price[0]:.2f}")
